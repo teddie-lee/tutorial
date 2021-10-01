0b6fcc8af5901596ad0c3fee0453c6d05b3ca42d
litaida <litaida@huya.com>
2021-10-01 23:21:03 +0800

    init commit

---
+++ chapter_2/chaper_2.py
@@ -0,0 +1,24 @@
+"""
+本次需要掌握的内容有：
+1. 字符串 string
+2. 列表 list
+3. 元组 tuple
+4. 字典 dict
+5. 集合 set
+6. 初步认识GitHub工作流 GitHub workflow
+"""
+
+"""
+字符串是编程语言中用于表示文本的数据类型，我们需要了解在python中如何创建和操纵字符串
+列表/元组/字典/集合都是python内置的数据结构（data structure，什么是数据结构：https://zhuanlan.zhihu.com/p/43859497）
+每种数据结构都有其对应的特性与应用场景，比如：dict维护了key -> value的映射关系，在编写学生管理系统时，我们可以很轻松地通过学号找到对应的
+学生姓名，只需将key作为学号，value作为姓名即可
+我们需要初步认识上述四种数据结构，了解它们创建/修改/访问/删除的相关API（什么是API：https://www.zhihu.com/question/38594466/answer/487039892，
+在教程中的体现就是各数据结构的内置函数）
+本次的作业无需编写一个可运行的小程序，把你的疑问通过注释以GitHub workflow的形式上传吧  https://zhuanlan.zhihu.com/p/39148914
+tips：使用Squash and merge
+"""
+
+# your comment here
+
+
